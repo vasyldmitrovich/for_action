2813d03104f8ad70d7bb28c64cb5ffa23e629901
vasyldmitrovich <48633322+vasyldmitrovich@users.noreply.github.com>
2025-01-11 23:35:04 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Hello from bash file"
-echo ${pwd}
-echo ${ls -a}
+echo $(pwd)
+echo $(ls -a)
 touch temp.txt
 echo "Text in temp.txt file" > temp.txt
+fold=`pwd`
+echo "Folder is: ${fold}" >> temp.txt
+cat temp.txt
 echo "Finish"
